aafd5f28365a88a2d8799bd1ab2d2de1532298de
milanr22 <milanrawal@fintech.com.np>
2024-06-07 15:24:01 +0545

    styling updated

--- clients/src/components/Header.tsx
+++ clients/src/components/Header.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { GiUnstableProjectile } from 'react-icons/gi';
+import React from "react";
+import { GiUnstableProjectile } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <nav className='navbar py-3 bg-blue-400 pl-2 navbar-expand-lg navbar-light bg-light'>
-      <div className='container flex flex-row '>
-        <a className='navbar-brand  flex flex-row gap-4 ' href='/'>
+    <nav className="navbar py-3 bg-blue-400 pl-2 navbar-expand-lg navbar-light bg-light">
+      <div className="container text-white flex flex-row ">
+        <a className="navbar-brand  flex flex-row gap-4 " href="/">
           <GiUnstableProjectile fontSize={30} />
-          <p className='text-3xl  fo'>Project Management</p>
+          <p className="text-3xl  font-semibold">Project Management</p>
         </a>
       </div>
     </nav>
--- clients/src/pages/Home.tsx
+++ clients/src/pages/Home.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import ClientModal from '../components/Modal/ClientModal';
+import ClientModal from "../components/Modal/ClientModal";
 
-import ProjectModal from '../components/Modal/ProjectModal';
+import ProjectModal from "../components/Modal/ProjectModal";
 
-import Clients from '../components/Clients';
-import { Projects } from '../components/Projects';
+import Clients from "../components/Clients";
+import { Projects } from "../components/Projects";
 
 const Home = () => {
   return (
     <div>
-      <div className='container my-2 pl-3'>
-        <ClientModal title='Add Client' />
+      <div className="container my-2 pl-3">
+        <ClientModal title="Add Client" />
 
-        <ProjectModal title='Add Project' />
+        <ProjectModal title="Add Project" />
       </div>
-      <div className=''>
-        <Clients />
+      <div className="">
         <Projects />
+
+        <Clients />
       </div>
     </div>
   );
